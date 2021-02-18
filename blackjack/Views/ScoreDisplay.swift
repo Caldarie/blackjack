e0cbc6defb7a243abfac7bebfe4d4861034f73b2
Caldarie <michael_nguyen4@hotmail.com>
2021-02-18 23:28:17 +0900

    Show UI for score

--- blackjack/Views/ScoreDisplay.swift
+++ blackjack/Views/ScoreDisplay.swift
@@ -1,8 +1,40 @@
 //
-//  ScoreDisplay.swift
+//  ResultDisplay.swift
 //  blackjack
 //
-//  Created by Michael Nguyen on 2021/02/18.
+//  Created by Michael Nguyen on 2021/02/13.
 //
 
-import Foundation
+import SwiftUI
+
+struct ScoreDisplay: View {
+    
+    @Binding var player: Status
+    @Binding var dealer: Status
+
+    
+    var body: some View {
+        HStack {
+            
+            Text(
+                String(player.totalWins)
+            )
+            .fixedSize()
+            .font(.system(size: 72))
+            
+            Spacer()
+                .frame(width: 20)
+            
+            Text(
+                String(dealer.totalWins)
+            )
+            .fixedSize()
+            .font(.system(size: 72))
+            
+        }
+    }
+    
+    
+    
+}
+
