30d744d1ebb49452485880cd10fc5725eeef5215
Caldarie <michael_nguyen4@hotmail.com>
2021-02-18 23:24:31 +0900

    Initialise values

--- blackjack/Models/Status.swift
+++ blackjack/Models/Status.swift
@@ -12,4 +12,13 @@ struct Status: Decodable {
     var totalWins: Int
     var hand: [Deck]?
     var totalCardScore: Int
+    
+    init(totalWins: Int = 0,
+         hand: [Deck]? = [],
+         totalCardScore: Int = 0){
+        
+        self.totalWins = totalWins
+        self.hand = hand
+        self.totalCardScore = totalCardScore
+    }
 }
