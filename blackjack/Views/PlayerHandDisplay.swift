8dfdd529d2c8723b2bed3033c7688eba6f716b4f
Caldarie <michael_nguyen4@hotmail.com>
2021-02-18 23:27:58 +0900

    Allow ui to dynamically show players hand

--- blackjack/Views/PlayerHandDisplay.swift
+++ blackjack/Views/PlayerHandDisplay.swift
@@ -11,20 +11,26 @@ import SwiftUI
 struct PlayerHandDisplay: View {
 
     @Binding var player: Status
+    
+    private let vm = HandDisplayViewModel()
 
     var body: some View {
-//        GeometryReader { g in
-//          HStack {
-//            Text("Overview").frame(width: g.size.width / 3)
-//            Text("Bio").frame(width: g.size.width / 3)
-//            Text("Location").frame(width: g.size.width / 3)
-//          }
-//        }
-        HStack {
-            Spacer().overlay(Text("OverView").bold())
-            Spacer().overlay(Text("Bio").bold())
-            Spacer().overlay(Text("Location").bold())
-
+      HStack {
+        ForEach(ArrayOfCardImages(), id: \.self) { value in
+            Image(uiImage: UIImage(named: value)!)
+                .resizable()
+                .frame(width: 99, height: 153)
+                .aspectRatio(contentMode: .fit)
+                .layoutPriority(1)
         }
+     }
     }
+    
+
+    func ArrayOfCardImages() -> [String]{
+        let imageLocations = vm.getImageLocations(hand: player.hand!)
+        return imageLocations
+    }
+    
+    
 }
