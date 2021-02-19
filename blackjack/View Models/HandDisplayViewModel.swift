106dbed61df8c24b154b1947c08de2479304d002
Caldarie <michael_nguyen4@hotmail.com>
2021-02-19 18:43:54 +0900

    Add function to dynamically adjust card to centre

--- blackjack/View Models/HandDisplayViewModel.swift
+++ blackjack/View Models/HandDisplayViewModel.swift
@@ -6,10 +6,11 @@
 //
 
 import Foundation
-
+import SwiftUI
 
 class HandDisplayViewModel{
     
+    //retreive a list of image locations from Deck
     public func getImageLocations(hand: [Deck]) -> [String]{
         var stringLoc: [String] = []
         for i in hand{
@@ -17,5 +18,22 @@ class HandDisplayViewModel{
         }
         print(stringLoc)
         return stringLoc
+    }
+    
+    //Adjust cards to centre whenver the hand increases
+    func cardAlignment(numOfCards: Int, index: Int) -> CGFloat{
+        var offsetX: CGFloat
+        
+        if(numOfCards == 2){
+            offsetX = -30.0 + (CGFloat(index) * 50.0)
+        }else if(numOfCards == 3){
+            offsetX = -50.0 + (CGFloat(index) * 50.0)
+        }else if(numOfCards == 4){
+            offsetX = -70.0 + (CGFloat(index) * 50.0)
+        }else{
+            offsetX = -90.0 + (CGFloat(index) * 50.0)
+        }
+        
+        return offsetX
     }
 }
