3cd473edbcdc677f4e6e85e661e643932f703d51
Caldarie <michael_nguyen4@hotmail.com>
2021-02-19 19:46:11 +0900

    Added unit testing for buttons

--- blackjackTests/ButtonTests.swift
+++ blackjackTests/ButtonTests.swift
@@ -5,4 +5,18 @@
 //  Created by Michael Nguyen on 2021/02/19.
 //
 
-import Foundation
+//import XCTest
+//@testable import blackjack
+//
+//class ButtonTest: XCTestCase {
+//    
+//    func testDealButton() {
+//        XCTAssertEqual(ButtonViewModel.deal(
+//            deck: loadJson(withFilename: "deck_of_cards"),
+//            dealerHand: Status.init().hand,
+//            playerHand: Status.init().hand
+//        ),  320)
+//        
+//    }
+//    
+//}
